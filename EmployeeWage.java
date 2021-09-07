685b46512784d71e3fd781f3055833efd36c5f1f
sushant2931 <sushantlad8888@gmail.com>
2021-09-08 02:18:43 +0530

    Check Emplyoee Parttime And Fulltime

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,18 +5,22 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Builder Program For Multiple companies ");
-		int IS_FULL_TIME = 1;
-		int EMP_RATE_PER_HOUR = 20;
-		int empHrs = 0;
+			int IS_FULL_TIME = 1;
+			int IS_PART_TIME = 2;
+			int EMP_RATE_PER_HOUR = 20;
+			int empHrs = 0;
 
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if (empCheck == IS_FULL_TIME) {
 			System.out.println("employee is present");
 			empHrs = 8;
-		} else {
-			System.out.println("employee is absent");
-			empHrs = 0;
 		}
+		else if (empCheck == IS_PART_TIME) {
+			System.out.println("employee is absent");
+			empHrs = 4;
+		} 
+		else
+			empHrs = 0;
 		int empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage " + empWage);
 	}
