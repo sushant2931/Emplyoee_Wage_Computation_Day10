5eff429b5f13dc3b25588f7a72d31c8afc60fb0e
sushant2931 <sushantlad8888@gmail.com>
2021-09-08 02:21:18 +0530

    Emplyoee Wage Using switch Case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,27 +1,27 @@
 package com.bl.emplyoeewage;
 
 public class EmployeeWage {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Builder Program For Multiple companies ");
-			int IS_FULL_TIME = 1;
-			int IS_PART_TIME = 2;
-			int EMP_RATE_PER_HOUR = 20;
-			int empHrs = 0;
-
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_FULL_TIME) {
-			System.out.println("employee is present");
-			empHrs = 8;
-		}
-		else if (empCheck == IS_PART_TIME) {
-			System.out.println("employee is absent");
+		int empHrs = 0;
+		int empWage = 0;
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+		switch (empCheck) {
+		case IS_PART_TIME:
 			empHrs = 4;
-		} 
-		else
+			break;
+		case IS_FULL_TIME:
+			empHrs = 8;
+			break;
+		default:
 			empHrs = 0;
-		int empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage " + empWage);
+		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage=" + empWage);
 	}
 }
