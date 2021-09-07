65b0d973f4a513b19b485c6ce527a908e92b36fb
sushant2931 <sushantlad8888@gmail.com>
2021-09-08 02:15:46 +0530

    Check Daily Wage Calculation

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,13 +6,18 @@ public class EmployeeWage {
 
 		System.out.println("Welcome to Employee Wage Builder Program For Multiple companies ");
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME) {
 			System.out.println("employee is present");
-		}
-		else
-		{
+			empHrs = 8;
+		} else {
 			System.out.println("employee is absent");
+			empHrs = 0;
 		}
+		int empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage " + empWage);
 	}
 }
