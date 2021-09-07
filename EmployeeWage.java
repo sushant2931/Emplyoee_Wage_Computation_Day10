3f77f7f3de5460485d5ab4939d4fd1cfcf438ee0
sushant2931 <sushantlad8888@gmail.com>
2021-09-08 02:09:06 +0530

    Welcome In Emplyoee Wage Compuatation Program On Master Branch

---
+++ EmployeeWage.java
@@ -0,0 +1,7 @@
+package com.bl.emplyoeewage;
+
+public class EmployeeWage {
+	public static void main(String[] args) {
+		System.out.println("Welcome in Emplyoee Wage Computation Program On Master Branch ");
+	}
+}
