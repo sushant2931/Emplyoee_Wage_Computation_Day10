3ca322673077ffb44c575676391e87ce661247b8
sushant2931 <sushantlad8888@gmail.com>
2021-09-08 02:13:24 +0530

    Check Emplyoee Attendence

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,18 @@
 package com.bl.emplyoeewage;
 
 public class EmployeeWage {
+
 	public static void main(String[] args) {
-		System.out.println("Welcome in Emplyoee Wage Computation Program On Master Branch ");
+
+		System.out.println("Welcome to Employee Wage Builder Program For Multiple companies ");
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("employee is present");
+		}
+		else
+		{
+			System.out.println("employee is absent");
+		}
 	}
 }
