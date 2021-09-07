36887de67dcbec3e7f1349eb50491c5941d29b85
sushant2931 <sushantlad8888@gmail.com>
2021-09-08 02:40:08 +0530

    Total wage For Each Company

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,9 +4,22 @@ public class EmployeeWage {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 
-	public int computeEmpWage(String company, int empRatePerHr, int numWorkingDays, int maxHrs) {
-		int empHrs = 0, totalEmpHrs = 8, totalWorkingDays = 0;
-		while (totalEmpHrs < maxHrs && totalWorkingDays < numWorkingDays) {
+	private final String company;
+	private final int empRatePerHr;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
+
+	public EmployeeWage(String company, int empRatePerHr, int numOfWorkingDays, int maxHoursPerMonth) {
+		this.company = company;
+		this.empRatePerHr = empRatePerHr;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+
+	public void computeEmpWage() {
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		while (totalEmpHrs < maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
@@ -20,22 +33,25 @@ public class EmployeeWage {
 				empHrs = 0;
 			}
 			totalEmpHrs += empHrs;
-			// System.out.println("Day:" + totalWorkingDays + " Emp Hr:" + empHrs);
+			System.out.println("Day:" + totalWorkingDays + " Emp Hr:" + empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * empRatePerHr;
-		System.out.println(company + "\n company Total Daily Wage" + " for days " + totalWorkingDays + " and hours "
-				+ totalEmpHrs + "  is:  " + totalEmpWage + "\n");
-		return totalEmpWage;
+		totalEmpWage = totalEmpHrs * empRatePerHr;
+	}
+
+	public String toString() {
+		return ("Total emp Wage for company :" + company + " is: " + totalEmpWage);
 	}
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Builder Program For Multiple companies ");
-		System.out.println("\n Welcome to EmployeeWage builder \n");
-		EmployeeWage dMart = new EmployeeWage();
-		dMart.computeEmpWage("TCS", 20, 20, 100);
-		EmployeeWage relianceDigital = new EmployeeWage();
-		relianceDigital.computeEmpWage("Accenture", 30, 26, 150);
-
+		 System.out.println("\n Welcome to EmployeeWage builder \n");
+	        EmployeeWage dMart = new EmployeeWage("TCS", 20, 20, 100);
+	        EmployeeWage reliance = new EmployeeWage("Accenture", 10, 20, 110);
+	        dMart.computeEmpWage();
+	        System.out.println(dMart);
+	        reliance.computeEmpWage();
+	        System.out.println(reliance);
+	    }
 	}
-}
+
