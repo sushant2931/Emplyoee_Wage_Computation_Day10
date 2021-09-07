ab51c05caee59c021b57a6b61232e5568adea556
sushant2931 <sushantlad8888@gmail.com>
2021-09-08 02:23:43 +0530

    Daily Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -10,18 +10,24 @@ public class EmployeeWage {
 		System.out.println("Welcome to Employee Wage Builder Program For Multiple companies ");
 		int empHrs = 0;
 		int empWage = 0;
-		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
-		switch (empCheck) {
-		case IS_PART_TIME:
-			empHrs = 4;
-			break;
-		case IS_FULL_TIME:
-			empHrs = 8;
-			break;
-		default:
-			empHrs = 0;
+		int numberOfWorkingDays = 20;
+		int totalWage = 0;
+		for (int i = 1; i <= numberOfWorkingDays; i++) {
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+			switch (empCheck) {
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("day" + i + "Employee Wage=" + empWage);
+			totalWage += empWage;
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Employee Wage=" + empWage);
+		System.out.println("Total wage of Employee for 20days:" + totalWage);
 	}
 }
